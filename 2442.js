759986b3a5e0f197437c72fc488b8ac12787810e
jh8057 <luckysymbol13@gmail.com>
2022-08-24 23:52:16 +0900

    2442

---
+++ 2442.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = Number(fs.readFileSync("./input/2741.txt").toString());
+let answer = "";
+for (let i = 1; i <= input; i++) {
+  answer +=
+    " ".repeat(input - i) +
+    "*".repeat(2 * i - 1) +
+    " ".repeat(input - i) +
+    "\n";
+}
+
+console.log(answer);
