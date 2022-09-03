6f4714371c3f20586eaf9958da5dccc9a960e85c
jh8057 <luckysymbol13@gmail.com>
2022-09-03 23:00:31 +0900

    1463

---
+++ 1463.js
@@ -0,0 +1,6 @@
+let fs = require("fs");
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync("./input/2741.txt").toString();
+let answer = "";
+
+//..
