a84f16b65a880e47c8ac4529bfb2a7e0e93d067a
jh8057 <luckysymbol13@gmail.com>
2022-09-08 23:54:19 +0900

    1463

--- 1463.js
+++ 1463.js
@@ -1,6 +1,21 @@
 let fs = require("fs");
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = fs.readFileSync("./input/2741.txt").toString();
-let answer = "";
 
-//..
+let cnt = 0;
+
+while (input !== 1) {
+  if (input % 3 === 0) {
+    input = input / 3;
+  } else if (input % 3 === 1) {
+    input = input - 1;
+  } else if (input % 2 === 0) {
+    input = input / 2;
+  } else {
+    input = input - 1;
+  }
+  cnt++;
+  console.log(cnt, input);
+}
+
+console.log(cnt);
