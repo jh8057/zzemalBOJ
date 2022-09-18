25c2b7a817f6c6f1810d3765273837e1d4f817da
jh8057 <luckysymbol13@gmail.com>
2022-09-19 01:06:26 +0900

    11726

--- 11726.js
+++ 11726.js
@@ -1,4 +1,16 @@
 let fs = require("fs");
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = Number(fs.readFileSync("./input/2741.txt").toString());
-const dp = new Array(input + 1).fill(0);
+const dp = {
+  1: 1,
+  2: 2,
+};
+
+for (let i = 3; i <= input; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+// console.log(dp);
+console.log(dp[input]);
+
+// https://kosaf04pyh.tistory.com/222
