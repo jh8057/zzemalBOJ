842aec5bad3bfaf580ec173766a45f01d4168e05
jh8057 <luckysymbol13@gmail.com>
2022-09-18 23:45:20 +0900

    11727

--- 11727.js
+++ 11727.js
@@ -2,3 +2,5 @@ let fs = require("fs");
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = Number(fs.readFileSync("./input/2741.txt").toString());
 const dp = new Array(input + 1).fill(0);
+
+//
