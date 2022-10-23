0631a0ae23c1cca473b4a4329fc1e880286387a8
jh8057 <luckysymbol13@gmail.com>
2022-10-24 01:34:17 +0900

    9095

---
+++ 9095_re.js
@@ -0,0 +1,27 @@
+let fs = require("fs");
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync("./input/9095.txt").toString().split("\n");
+let answer = 0;
+let goal = 0;
+let result = "";
+
+function add(sum) {
+  if (sum > goal) return;
+  if (sum == goal) {
+    answer++;
+    return;
+  }
+
+  add(sum + 1);
+  add(sum + 2);
+  add(sum + 3);
+}
+
+for (let i = 1; i <= input[0]; i++) {
+  answer = 0;
+  goal = input[i];
+  add(0);
+  result += answer + "\n";
+}
+
+console.log(result.trim());
