74960a1ef362253098ee4244fcfb28e1dad3a740
jh8057 <luckysymbol13@gmail.com>
2022-11-15 01:12:02 +0900

    11057

--- 11057.js
+++ 11057.js
@@ -1,3 +1,5 @@
 let fs = require("fs");
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = Number(fs.readFileSync("./input/10844.txt").toString().trim());
+
+//https://animoto1.tistory.com/entry/%EB%B0%B1%EC%A4%80-11057-%EC%98%A4%EB%A5%B4%EB%A7%89-%EC%88%98-%ED%8C%8C%EC%9D%B4%EC%8D%AC-Python
