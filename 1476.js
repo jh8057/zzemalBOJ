a4a22873ebd32634289d796d46a49a657cbf756d
jh8057 <luckysymbol13@gmail.com>
2022-11-20 01:27:19 +0900

    1476

--- 1476.js
+++ 1476.js
@@ -1,19 +1,20 @@
 let fs = require("fs");
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = fs.readFileSync("./input/1476.txt").toString();
-let [E, S, M] = input.split(" ");
-E = Number(E);
-S = Number(S);
-M = Number(M);
+
+let [E, S, M] = input.split(" ").map(Number);
 
 let target = 1;
 while (true) {
+  if (
+    (target - E) % 15 === 0 &&
+    (target - S) % 28 === 0 &&
+    (target - M) % 19 === 0
+  ) {
+    console.log(target);
+    return;
+  }
   target++;
-  let e = target % 15 === 0 ? 15 : target % 15;
-  let s = target % 28 === 0 ? 28 : target % 28;
-  let m = target % 19 === 0 ? 19 : target % 19;
-
-  if (E === e && S === s && M === m) break;
 }
 
 console.log(target);
