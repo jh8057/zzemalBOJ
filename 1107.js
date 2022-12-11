e987333a89d5b96ea647b10ee56f590032ea5a57
jh8057 <luckysymbol13@gmail.com>
2022-12-11 22:47:58 +0900

    1107

--- 1107.js
+++ 1107.js
@@ -3,16 +3,5 @@ let fs = require("fs");
 let input = Number(fs.readFileSync("./input/2741.txt").toString());
 const dp = new Array(input + 1).fill(0);
 
-for (let i = 2; i < dp.length; i++) {
-  dp[i] = dp[i - 1] + 1;
-
-  if (i % 3 === 0) {
-    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-  }
-  if (i % 2 === 0) {
-    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-  }
-}
-
 console.log(dp[input]);
 //https://webruden.tistory.com/1035
