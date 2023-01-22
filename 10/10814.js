503e7bc71aefa49f8daeecbe4ce354c85319891f
jh8057 <luckysymbol13@gmail.com>
2023-01-23 00:59:44 +0900

    10814

--- 10/10814.js
+++ 10/10814.js
@@ -18,3 +18,7 @@ for (let i = 0; i < N; i += 1) {
   res[age] += input[i] + "\n";
 }
 console.log(res);
+
+//안정 정렬(stable sort)
+// 즉, 정렬을 했을 때 중복된 값들의 순서가 변하지 않으면 안정(Stable) 정렬, 변하면 불안정(Unstable) 정렬인 것이다.
+// https://velog.io/@cookncoding/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B0%9C%EB%85%90-Stable-Sort-Inplace
