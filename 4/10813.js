aa91d4f7dc23f623259238b47bfc0c0f941f08a4
jh8057 <luckysymbol13@gmail.com>
2023-03-13 23:45:31 +0900

    10813

---
+++ 4/10813.js
@@ -0,0 +1,17 @@
+const input = require("fs")
+  .readFileSync("../input/10813.txt")
+  .toString()
+  .split("\n");
+
+const [N, M] = input[0].split(" ").map((el) => +el);
+const arr = new Array(N).fill().map((_, i) => {
+  return i + 1;
+});
+
+for (let i = 1; i <= M; i++) {
+  let [start, end] = input[i].split(" ");
+  let temp = arr[start - 1];
+  arr[start - 1] = arr[end - 1];
+  arr[end - 1] = temp;
+}
+console.log(arr.join(" "));
