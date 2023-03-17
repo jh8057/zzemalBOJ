fcdc8ed9fd93627ca7ade5162d599f6d5cdabd1a
jh8057 <luckysymbol13@gmail.com>
2023-03-17 23:58:12 +0900

    2231

--- 12/2231.js
+++ 12/2231.js
@@ -1,16 +1,14 @@
 const input = require("fs").readFileSync("../input/2231.txt").toString().trim();
 
-console.log(input);
-
 let LEN = input.length;
 
 let target = Number(input);
 for (let i = target - 9 * LEN; i < target + 9 * LEN; i++) {
   let result = i;
   let arr = String(i).split("");
-  arr.forEach((el) => (result += el));
+  arr.forEach((el) => (result += +el));
   if (result === target) {
-    console.log(result);
+    console.log(i);
     return;
   }
 }
