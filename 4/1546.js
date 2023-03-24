a2e4f28430a8b4b34d747d305d4e9950a27ea607
jh8057 <luckysymbol13@gmail.com>
2023-03-24 23:32:40 +0900

    1546

--- 4/1546.js
+++ 4/1546.js
@@ -1,11 +1,14 @@
-const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const input = require("fs")
+  .readFileSync("../input/1546.txt")
+  .toString()
+  .split("\n");
 
-const [N,arr] = input
-const scores = arr.split(' ').map(el=>Number(el))
-const max = Math.max(...scores)
-let answer = 0
-for(let i =0;i< N;i++){
-    answer += scores[i] / max * 100
+const [N, arr] = input;
+const scores = arr.split(" ").map((el) => Number(el));
+const max = Math.max(...scores);
+let answer = 0;
+for (let i = 0; i < N; i++) {
+  answer += (scores[i] / max) * 100;
 }
 
-console.log(Number(answer/N).toFix(1))
+console.log(parseFloat(answer / N));
