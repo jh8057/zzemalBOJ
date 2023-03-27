66357a64cbd1b18e6b7a0ce5b0873a75358fc9f4
jh8057 <luckysymbol13@gmail.com>
2023-03-27 23:55:51 +0900

    1018

--- 11/1018.js
+++ 11/1018.js
@@ -1,4 +1,8 @@
 const input = require("fs")
-  .readFileSync("../input/25501.txt")
+  .readFileSync("../input/1018.txt")
   .toString()
   .split("\n");
+
+const [M, N] = input.shift().split(" ");
+
+console.log(input, M, N);
