28c6769eddbde02edc4474a4820526f31f5f1bc2
jh8057 <luckysymbol13@gmail.com>
2023-03-30 00:14:19 +0900

    10809

--- 5/10809.js
+++ 5/10809.js
@@ -1,6 +1,24 @@
-const input = require("fs")
-  .readFileSync("../input/9086.txt")
-  .toString()
-  .split("\n");
+// const input = require("fs")
+//   .readFileSync("../input/10809.txt")
+//   .toString()
+//   .trim();
 
-for (let i = 0; i < 26; i++) {}
+// let answer = new Array(26).fill(-1);
+// for (let i = 0; i < input.length; i++) {
+//   let alpha = input[i].charCodeAt(0);
+//   let index = alpha - 97;
+
+//   answer[index] = answer[index] > 0 ? answer[index] : i;
+// }
+
+// console.log(answer.join(" "));
+
+const input = require("fs").readFileSync("/dev/stdin").toString();
+
+const result = [];
+
+for (let i = 97; i <= 122; i++) {
+  result.push(input.indexOf(String.fromCharCode(i)));
+}
+
+console.log(result.join(" "));
