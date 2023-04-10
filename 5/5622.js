56012994bb175547a75c1bf4bc9a42e69873b6e0
jh8057 <luckysymbol13@gmail.com>
2023-04-11 00:29:03 +0900

    5622

--- 5/5622.js
+++ 5/5622.js
@@ -1,4 +1,22 @@
-const input = require("fs")
-  .readFileSync("../input/2908.txt")
-  .toString()
-  .split(" ");
+const input = require("fs").readFileSync("../input/5622.txt").toString().trim();
+
+let dic = {
+  3: "ABC",
+  4: "DEF",
+  5: "GHI",
+  6: "JKL",
+  7: "MNO",
+  8: "PQRS",
+  9: "TUV",
+  10: "WXYZ",
+};
+let answer = 0;
+for (let i = 0; i < input.length; i++) {
+  for (const value in dic) {
+    if (dic[value].includes(input[i])) {
+      answer += +value;
+    }
+  }
+}
+
+console.log(answer);
