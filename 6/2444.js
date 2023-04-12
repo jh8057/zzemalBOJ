7a7a8c89b76456831688c572829f8d0ed1c149f7
jh8057 <luckysymbol13@gmail.com>
2023-04-13 01:04:27 +0900

    2444

--- 6/2444.js
+++ 6/2444.js
@@ -1,4 +1,13 @@
-const input = require("fs")
-  .readFileSync("../input/2675.txt")
-  .toString()
-  .split("\n");
+const input = Number(
+  require("fs").readFileSync("../input/2444.txt").toString()
+);
+
+let answer = "";
+for (let i = 1; i <= input; i++) {
+  answer += " ".repeat(input - i) + "*".repeat(2 * i - 1) + "\n";
+}
+
+for (let i = 1; i < input; i++) {
+  answer += " ".repeat(i) + "*".repeat(2 * (input - i) - 1) + "\n";
+}
+console.log(answer.trimEnd());
