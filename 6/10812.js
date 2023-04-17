2efb75106f2f0b479e6c340960c6c5c9acdc2e0c
jh8057 <luckysymbol13@gmail.com>
2023-04-17 23:58:11 +0900

    10812

--- 6/10812.js
+++ 6/10812.js
@@ -3,5 +3,17 @@ const input = require("fs")
   .toString()
   .split("\n");
 
-let [N, M] = input[0];
+let [N, M] = input.shift().split(" ");
+console.log(N, M);
+let basket = new Array(+N).fill(0).map((el, idx) => idx + 1);
+
+console.log(basket);
 console.log("input", input);
+
+input.forEach((el) => {
+  let [start, end, mid] = el.split(" ");
+  let target = basket.slice(start - 1, mid - 1);
+  let target2 = basket.slice(mid - 1, end);
+  console.log(target, target2);
+  console.log(basket.slice(0, 0));
+});
