95a0e49a82c2aa38b45ea8bdcc6345c49e7ed1f0
jh8057 <luckysymbol13@gmail.com>
2023-04-19 22:33:43 +0900

    2738

--- 7/2738.js
+++ 7/2738.js
@@ -21,3 +21,24 @@ for (let i = 0; i < N; i++) {
 }
 
 console.log(result.join("\n").trim());
+
+// const [[row, col], ...inputs] = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .trim()
+//   .split("\n")
+//   .map((line) => line.split(" ").map(Number));
+
+// const A = inputs.slice(0, row);
+// const B = inputs.slice(row);
+
+// for (let r = 0; r < row; r++) {
+//   for (let c = 0; c < col; c++) {
+//     A[r][c] += B[r][c];
+//   }
+// }
+
+// const sol = [];
+// A.forEach((line) => sol.push(line.join(" ")));
+
+// console.log(sol.join("\n"));
