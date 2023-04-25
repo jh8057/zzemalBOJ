35ac4a3c4ce01e2a725f799681bd51ed9eb0ad51
jh8057 <luckysymbol13@gmail.com>
2023-04-25 23:57:53 +0900

    11659

--- 24/11659.js
+++ 24/11659.js
@@ -2,4 +2,8 @@ const input = require("fs")
   .readFileSync("../input/11659.txt")
   .toString()
   .trim()
-  .split(" ");
+  .split("\n");
+
+console.log(input);
+const [N, M] = input.shift().split(" ");
+console.log(N, M);
