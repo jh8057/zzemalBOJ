b1a04a8c074ed10b26c10543757966e7b5a10ee9
jh8057 <luckysymbol13@gmail.com>
2023-04-26 00:52:46 +0900

    11659

--- 24/11659.js
+++ 24/11659.js
@@ -4,6 +4,26 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-console.log(input);
-const [N, M] = input.shift().split(" ");
-console.log(N, M);
+const [N, M] = input
+  .shift()
+  .split(" ")
+  .map((el) => +el);
+
+let arr = input
+  .shift()
+  .split(" ")
+  .map((el) => +el);
+
+let sum = new Array(N + 1).fill(0);
+for (let i = 1; i <= N; i++) {
+  sum[i] = sum[i - 1] + arr[i - 1];
+}
+
+let answer = [];
+for (let i = 0; i < M; i++) {
+  let [start, end] = input[i].split(" ").map((el) => +el);
+  let result = sum[end] - sum[start - 1];
+  answer.push(result);
+}
+
+console.log(answer.join("\n").trim());
