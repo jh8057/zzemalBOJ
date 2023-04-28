a1b17aa6385a522e853447499bff31ec4b9e04e9
jh8057 <luckysymbol13@gmail.com>
2023-04-29 00:03:30 +0900

    1157

--- 6/1157.js
+++ 6/1157.js
@@ -19,3 +19,23 @@ let value = Object.values(obj);
 let max = Math.max(...value);
 if (value.filter((el) => el === max).length !== 1) console.log("?");
 else console.log(key[value.indexOf(max)]);
+
+// const result = new Array(26).fill(0);
+
+// for (let i = 0; i < input.length; i++) {
+//   result[input.charCodeAt(i) - 97]++;
+// }
+
+// const max = Math.max(...result); // 2
+// const index = result.indexOf(max); // 26
+
+// let isSame = false;
+
+// for (let j = 0; j < 26; j++) {
+//   if (result[j] === max && index != j) {
+//     isSame = true;
+//     break;
+//   }
+// }
+
+// console.log(isSame ? "?" : String.fromCharCode(index + 65));
