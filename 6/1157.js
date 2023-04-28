ba3faf7eca9defe7e503cf44b5cffa7665d17f3b
jh8057 <luckysymbol13@gmail.com>
2023-04-29 00:01:22 +0900

    1157

--- 6/1157.js
+++ 6/1157.js
@@ -2,9 +2,7 @@ const input = require("fs")
   .readFileSync("../input/1157.txt")
   .toString()
   .trim()
-  .toLocaleLowerCase();
-
-console.log(input);
+  .toUpperCase();
 
 let obj = {};
 for (let i = 0; i < input.length; i++) {
@@ -14,9 +12,10 @@ for (let i = 0; i < input.length; i++) {
     obj[input[i]] = 1;
   }
 }
-console.log(obj);
 
-let arr = Object.values(obj);
-console.log(arr);
-let max = Math.max(...arr);
-if (arr.filter((el) => el === max).length !== 1) console.log("?");
+let key = Object.keys(obj);
+let value = Object.values(obj);
+
+let max = Math.max(...value);
+if (value.filter((el) => el === max).length !== 1) console.log("?");
+else console.log(key[value.indexOf(max)]);
