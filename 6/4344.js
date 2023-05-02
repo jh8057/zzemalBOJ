66a2331f8f79d4a06b460cbc1fe815e2b4bc94ec
jh8057 <luckysymbol13@gmail.com>
2023-05-02 23:16:42 +0900

    4344

--- 6/4344.js
+++ 6/4344.js
@@ -1,5 +1,17 @@
 const input = require("fs")
-  .readFileSync("../input/1157.txt")
+  .readFileSync("../input/4344.txt")
   .toString()
   .trim()
   .split("\n");
+
+const C = input.shift();
+
+for (let i = 0; i < C; i++) {
+  let arr = input[i].slice().split(" ");
+  let N = arr.shift();
+  let avg =
+    arr.reduce((prev, cur) => Number(prev) + Number(cur), 0) / arr.length;
+  let count = arr.filter((student) => student > avg).length;
+
+  console.log(((count / arr.length) * 100).toFixed(3) + "%");
+}
