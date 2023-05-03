9ce7a68d118c6db6862f3b95e3ab5ffd20ef850c
jh8057 <luckysymbol13@gmail.com>
2023-05-03 23:17:04 +0900

    2941

--- 6/2941.js
+++ 6/2941.js
@@ -1,1 +1,8 @@
-const input = require("fs").readFileSync("../input/1157.txt").toString().trim();
+let input = require("fs").readFileSync("../input/2941.txt").toString().trim();
+let alpha = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+
+for (let i = 0; i < alpha.length; i++) {
+  let target = alpha[i];
+  input = input.replaceAll(target, "*");
+}
+console.log(input.length);
