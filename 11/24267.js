239661e7d7b50a4aa20011bc6359e46334d7c367
jh8057 <luckysymbol13@gmail.com>
2023-05-07 02:03:43 +0900

    24267

--- 11/24267.js
+++ 11/24267.js
@@ -1,7 +1,12 @@
-const n = require("fs").readFileSync("../input/24265.txt").toString().trim();
+// const n = require("fs").readFileSync("../input/24265.txt").toString().trim();
 
-console.log(`${(n * (n - 1) * (n - 2)) / 6}`);
+// console.log(`${(n * (n - 1) * (n - 2)) / 6}`);
 
-console.log("3");
+// console.log("3");
 
 // https://dev-mandos.tistory.com/124
+
+const n = require("fs").readFileSync("/dev/stdin").toString().trim();
+const answer = (BigInt(n) * BigInt(n - 1) * BigInt(n - 2)) / BigInt(6);
+
+console.log(`${answer}\n3`);
