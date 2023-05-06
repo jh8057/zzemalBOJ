de27003afdf15b4b3b1d3866434c11dc06d203ea
jh8057 <luckysymbol13@gmail.com>
2023-05-07 01:14:32 +0900

    24267

--- 11/24267.js
+++ 11/24267.js
@@ -1,3 +1,7 @@
 const n = require("fs").readFileSync("../input/24265.txt").toString().trim();
-console.log(n ** 2 * (n - 2));
+
+console.log(`${(n * (n - 1) * (n - 2)) / 6}`);
+
 console.log("3");
+
+// https://dev-mandos.tistory.com/124
