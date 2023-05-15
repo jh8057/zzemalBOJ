24b995173da1cfe80e4c6d99c9cbcff2dcc09a2a
jh8057 <luckysymbol13@gmail.com>
2023-05-16 00:55:44 +0900

    2566

--- 7/2566.js
+++ 7/2566.js
@@ -4,7 +4,18 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-let [N, M] = input
-  .shift()
-  .split(" ")
-  .map((el) => +el);
+let max = -1;
+let location = [0, 0];
+let len = input[0].split(" ").length;
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < len; j++) {
+    let target = input[i].split(" ").map((el) => +el);
+    if (target[j] > max) {
+      max = target[j];
+      location = [i + 1, j + 1];
+    }
+  }
+}
+
+console.log(max);
+console.log(location[0], location[1]);
