e4d7449fc0a2c8360c692bba97cbd8184c314672
jh8057 <luckysymbol13@gmail.com>
2023-06-04 23:57:57 +0900

    2559

--- 24/2559.js
+++ 24/2559.js
@@ -2,3 +2,14 @@ const input = require("fs")
   .readFileSync("../input/2559.txt")
   .toString()
   .split("\n");
+
+console.log(input);
+
+const [K, N] = input.shift().split(" ");
+
+console.log(K, N);
+
+const arr = input.split(" ").map((el) => +el);
+const sum = arr.map(el, (i) => {
+  return el + (arr[i - 1] || 0);
+});
