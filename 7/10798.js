ac3100cd02a4ef77123e6ed4fdc2d0d9688c5de0
jh8057 <luckysymbol13@gmail.com>
2023-06-20 23:59:30 +0900

    10798

---
+++ 7/10798.js
@@ -0,0 +1,14 @@
+const input = require("fs")
+  .readFileSync("../input/10798.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+console.log("input0", input);
+
+let answer = "";
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < input.length; j++) {
+    input[j][i];
+  }
+}
