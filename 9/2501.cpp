944b55f5bd937f30825960bd0b6d19fbebe4f808
jh8057 <luckysymbol13@gmail.com>
2023-10-19 00:24:10 +0900

    2501

--- 9/2501.cpp
+++ 9/2501.cpp
@@ -26,3 +26,12 @@ int main()
 
     return 0;
 }
+
+// #include<cstdio>
+// int i,n,k;
+// int main() {
+//     scanf("%d %d", &n, &k);
+//     for (i = 1; i <= n&&k; i++) k -= !(n%i); // n을 i로 나눈 나머지가 0인 경우 !(n%i)의 값이 1이 되어 k에서 1을 빼게 됩니다.
+//     printf("%d", k ? 0 : i - 1);
+//     return 0;
+// }
