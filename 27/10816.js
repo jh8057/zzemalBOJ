0cfe746a3cbb8c18d9b2af50bc02bdc5868f36b5
jh8057 <luckysymbol13@gmail.com>
2024-03-30 12:48:34 +0900

    10816

--- 27/10816.js
+++ 27/10816.js
@@ -4,4 +4,23 @@ const inputs = require("fs")
   .trim()
   .split("\n");
 
-console.log("input", inputs);
+const N = Number(inputs[0]);
+const cards = inputs[1].split(" ").map((el) => +el);
+const M = Number(inputs[2]);
+const targets = inputs[3].split(" ").map((el) => +el);
+
+let cardCount = new Map();
+for (let i = 0; i < N; i++) {
+  let card = cards[i];
+  if (cardCount.has(card)) {
+    cardCount.set(card, cardCount.get(card) + 1);
+  } else {
+    cardCount.set(card, 1);
+  }
+}
+
+const answer = [];
+for (let i = 0; i < M; i++) {
+  answer.push(cardCount.get(targets[i]) || 0);
+}
+console.log(answer.join(" "));
