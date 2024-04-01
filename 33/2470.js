72fbb9f53446ea38488aa01b7342e909cd49ab1f
jh8057 <luckysymbol13@gmail.com>
2024-04-01 23:39:04 +0900

    2470

---
+++ 33/2470.js
@@ -0,0 +1,15 @@
+const inputs = require("fs")
+  .readFileSync("../input/2470.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+console.log("input", inputs);
+
+const N = +inputs[0];
+
+let startPoint = 0;
+let endPoint = 0;
+for (let i = 0; i < N; i++) {
+  inputs[1];
+}
