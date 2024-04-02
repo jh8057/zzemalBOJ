a40ec87a1112b4aa326dfcebb13b20137c72a558
jh8057 <luckysymbol13@gmail.com>
2024-04-02 23:59:10 +0900

    2470

--- 33/2470.js
+++ 33/2470.js
@@ -7,9 +7,10 @@ const inputs = require("fs")
 console.log("input", inputs);
 
 const N = +inputs[0];
+const arr = inputs[1].split(" ");
 
 let startPoint = 0;
 let endPoint = 0;
 for (let i = 0; i < N; i++) {
-  inputs[1];
+  console.log(arr[i]);
 }
