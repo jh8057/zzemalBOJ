1ce8633b27b9f2e79fcf603bffc2f03c952c0a2a
jh8057 <luckysymbol13@gmail.com>
2024-06-04 00:15:16 +0900

    2839

--- class/2/2839.js
+++ class/2/2839.js
@@ -2,18 +2,48 @@ const inputs = Number(
   require("fs").readFileSync("../../input/2839.txt").toString().trim()
 );
 
-console.log("input", inputs);
-
-let count = 0;
+let count5 = 0;
+let count3 = 0;
 
 let div = parseInt(inputs / 5);
-console.log(div);
-count += div;
+count5 += div;
 
 let extra = inputs % 5;
 
-console.log(extra);
-if (extra === 0) {
-  console.log(count);
-  return;
+while (count5 >= 0) {
+  if (extra === 0) {
+    console.log(count5 + count3);
+    return;
+  }
+
+  if (extra >= 3) {
+    count3 += parseInt(extra / 3);
+    extra = extra % 3;
+  } else if (extra < 3) {
+    count5--;
+    extra += 5;
+  }
 }
+
+if (count5 > -1 && extra === 0) console.log(count3);
+console.log(-1);
+
+// 참고
+// let rest = +input;
+// let result = 0;
+
+// if (rest % 5 == 0) {
+//   result += rest / 5;
+//   rest = 0;
+// } else {
+//   while (rest > 0) {
+//     if (rest % 5 != 0) {
+//       rest -= 3;
+//       result++;
+//     } else {
+//       result += rest / 5;
+//       rest = 0;
+//     }
+//   }
+// }
+// console.log(rest ? -1 : result);
