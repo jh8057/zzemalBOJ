7ab68162dd2f591af18297af08cb1366ca2761b8
jh8057 <luckysymbol13@gmail.com>
2024-06-12 23:39:00 +0900

    2579

--- class/3/2579.js
+++ class/3/2579.js
@@ -2,6 +2,25 @@ const inputs = require("fs")
   .readFileSync("../../input/2579.txt")
   .toString()
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((el) => +el);
 
-console.log(inputs);
+const N = +inputs.shift();
+const dp = [];
+
+for (let i = 0; i < N; i++) {
+  if (i === 0) {
+    dp[0] = inputs[i];
+  } else if (i === 1) {
+    dp[1] = dp[0] + inputs[i];
+  } else {
+    dp[i] = Math.max(
+      (dp[i - 3] || 0) + inputs[i - 1] + inputs[i],
+      dp[i - 2] + inputs[i]
+    );
+  }
+}
+
+console.log(dp.pop());
+
+// dp 뿐 아니라 input값도 잘 활용해서 발화식을 세우자
