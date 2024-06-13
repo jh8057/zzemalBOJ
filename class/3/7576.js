07bed460844e0d9d36364472d61f8220cdd5c3f2
jh8057 <luckysymbol13@gmail.com>
2024-06-13 23:58:34 +0900

    7576

---
+++ class/3/7576.js
@@ -0,0 +1,53 @@
+const inputs = require("fs")
+  .readFileSync("../../input/7576.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [M, N] = inputs
+  .shift()
+  .split(" ")
+  .map((el) => +el);
+
+let arr = inputs.map((el) => el.split(" ").map((el) => +el));
+let day = 0;
+
+let visited = [];
+
+while (true) {
+  console.log(arr);
+  let isChange = false;
+  let isDone = true;
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      if (arr[i][j] > 0 && !visited.includes([i, j])) {
+        for (let k = 0; k < 4; k++) {
+          let dx = [0, 1, 0, -1];
+          let dy = [1, 0, -1, 0];
+
+          let ny = dy[k] + i;
+          let nx = dx[k] + j;
+          if (arr[ny]?.[nx] === 0) {
+            arr[ny][nx] = 1;
+            isChange = true;
+            visited.push([ny, nx]);
+          }
+        }
+      } else if (arr[i][j] === 0) {
+        isDone = false;
+      }
+    }
+  }
+
+  if (isDone) {
+    console.log("isDone", day);
+    return day;
+  }
+  if (!isChange) {
+    console.log("impossible", -1);
+    return -1;
+  }
+  day++;
+
+  console.log("day", day);
+}
