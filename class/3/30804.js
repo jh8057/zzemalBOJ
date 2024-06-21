6c93ce882c9a2578299e3e4834225a6e38d3cd6d
jh8057 <luckysymbol13@gmail.com>
2024-06-21 23:59:22 +0900

    30804

--- class/3/30804.js
+++ class/3/30804.js
@@ -7,3 +7,17 @@ const inputs = require("fs")
 console.log(inputs);
 
 const N = +inputs.shift();
+const arr = inputs[0].split(" ").map((el) => +el);
+
+let dp = [];
+let obj = {};
+
+for (let i = 0; i < N; i++) {
+  if (obj[arr[i]]) {
+    dp[i] = dp[i - 1];
+  } else {
+    if (i === 0) dp[0] = 1;
+    else dp[i] = dp[i - 1] + 1;
+    obj[arr[i]] = true;
+  }
+}
