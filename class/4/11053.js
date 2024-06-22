f651a5ea853d54ee9467cd3a254a4a8c41c04f28
jh8057 <luckysymbol13@gmail.com>
2024-06-22 11:01:24 +0900

    11053

--- class/4/11053.js
+++ class/4/11053.js
@@ -3,3 +3,21 @@ const inputs = require("fs")
   .toString()
   .trim()
   .split("\n");
+
+let N = +inputs.shift();
+let arr = inputs
+  .pop()
+  .split(" ")
+  .map((el) => +el);
+
+let dp = Array(N).fill(1);
+
+for (let i = 1; i < N; i++) {
+  for (let j = 0; j < i; j++) {
+    if (arr[j] < arr[i]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+  }
+}
+
+console.log(Math.max(...dp));
