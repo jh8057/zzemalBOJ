e333bd426986b4b96cf4e5efddcbe28d656e8473
jh8057 <luckysymbol13@gmail.com>
2024-06-29 15:32:47 +0900

    15663

--- class/4/15663.js
+++ class/4/15663.js
@@ -4,4 +4,37 @@ const inputs = require("fs")
   .trim()
   .split("\n");
 
-console.log("input", inputs);
+const [N, M] = inputs
+  .shift()
+  .split(" ")
+  .map((el) => +el);
+
+let arr = inputs[0]
+  .split(" ")
+  .map((el) => +el)
+  .sort((a, b) => a - b);
+
+let visited = new Array(N).fill(false);
+let result = [];
+let answer = "";
+
+const dfs = (depth) => {
+  if (depth === M) {
+    answer += result.join(" ") + "\n";
+    return;
+  }
+  let previous = -1;
+  for (let i = 0; i < N; i++) {
+    if (visited[i] || previous === arr[i]) continue;
+    visited[i] = true;
+    result.push(arr[i]);
+    previous = arr[i];
+
+    dfs(depth + 1);
+
+    visited[i] = false;
+    result.pop();
+  }
+};
+dfs(0);
+console.log(answer.trim());
