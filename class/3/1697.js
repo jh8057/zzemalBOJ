295dcea32300e96d37241d8024e3fe44759fac81
jh8057 <luckysymbol13@gmail.com>
2024-07-02 23:59:24 +0900

    1679

--- class/3/1697.js
+++ class/3/1697.js
@@ -4,4 +4,29 @@ const inputs = require("fs")
   .trim()
   .split("\n");
 
-console.log(inputs);
+const [N, K] = inputs
+  .shift()
+  .split(" ")
+  .map((el) => +el);
+
+console.log(N, K);
+
+let count = 0;
+let value = N;
+let left = 0;
+while (true) {
+  count++;
+  value *= 2;
+  let half = parseInt(value / 2);
+  console.log("a", Math.abs(K - value), half);
+  if (Math.abs(K - value) < half) {
+    left = Math.abs(K - value);
+    break;
+  }
+}
+console.log(count, left);
+
+for (let i = count; i > 0; i--) {
+  let num = Math.pow(2, i);
+  console.log(num);
+}
