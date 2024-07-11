beabd4a28e875a017ea198f35e6a41a3d67452e3
jh8057 <luckysymbol13@gmail.com>
2024-07-11 23:44:14 +0900

    1149

--- class/4/1149.js
+++ class/4/1149.js
@@ -3,3 +3,31 @@ const inputs = require("fs")
   .toString()
   .trim()
   .split("\n");
+
+const N = Number(inputs.shift());
+const arr = inputs.map((el) => el.split(" ").map(Number));
+
+//초기값
+let maxNum = Infinity;
+let queue = [];
+for (let i = 0; i < 3; i++) {
+  queue.push([arr[0][i], i, 1]);
+}
+
+//dfs
+while (queue.length) {
+  const [sum, index, level] = queue.pop();
+  if (level === N) {
+    maxNum = Math.min(sum, maxNum);
+    continue;
+  }
+  for (let i = 0; i < 3; i++) {
+    if (i === index) continue;
+    else {
+      const next = sum + arr[level][i];
+      queue.push([next, i, level + 1]);
+    }
+  }
+}
+
+console.log(maxNum);
