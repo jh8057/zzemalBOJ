3a7dd2d1ea7baf1be3bfa5e7e70968f6582288fa
jh8057 <luckysymbol13@gmail.com>
2024-08-06 23:52:10 +0900

    12865

--- class/4/12865.js
+++ class/4/12865.js
@@ -4,13 +4,20 @@ const inputs = require("fs")
   .trim()
   .split("\n");
 
-console.log(inputs);
-
 let [N, K] = inputs
   .shift()
   .split(" ")
   .map((el) => +el);
 
 const arr = inputs.map((el) => el.split(" ").map(Number));
-arr.sort((a, b) => a[0] - b[0]);
-console.log(arr);
+
+const dp = new Array(K + 1).fill(0);
+
+for (let i = 0; i < N; i++) {
+  let [w, v] = arr[i];
+  for (let j = K; j - w >= 0; j--) {
+    dp[j] = Math.max(dp[j], dp[j - w] + v);
+  }
+}
+
+console.log(dp[K]);
