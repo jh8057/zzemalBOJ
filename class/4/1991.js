7ea9f1d7271eace963bb747963372329a69573c9
jh8057 <luckysymbol13@gmail.com>
2024-08-23 23:22:34 +0900

    1991

--- class/4/1991.js
+++ class/4/1991.js
@@ -8,88 +8,26 @@ const N = Number(inputs.shift());
 
 const map = new Map();
 
-class Node {
-  constructor({ root, left, right }) {
-    this.root = root;
-    this.left = left;
-    this.right = right;
-  }
-}
-
 for (let i = 0; i < N; i++) {
   let [root, left, right] = inputs[i].split(" ");
 
-  map[root] = new Node({ root, left, right });
+  map.set(root, { left, right });
 }
 
-// 전위
-const pre = (node, answer = "") => {
-  const root = node.root;
-  const left = node.left;
-  const right = node.right;
+const traverse = (type, node, answer = []) => {
+  if (node === ".") return answer;
 
-  //middle
-  answer += root;
-  //left
-  if (left !== ".") {
-    answer = pre(map[left], answer);
-  }
-  //right
-  if (right !== ".") {
-    answer = pre(map[right], answer);
-  }
+  const { left, right } = map.get(node);
+
+  if (type === "pre") answer.push(node);
+  traverse(type, left, answer);
+  if (type === "in") answer.push(node);
+  traverse(type, right, answer);
+  if (type === "post") answer.push(node);
 
   return answer;
 };
 
-const result = pre(map["A"], "");
-console.log(result);
-
-// 중위
-const middle = (node, answer = "") => {
-  const root = node.root;
-  const left = node.left;
-  const right = node.right;
-
-  //left
-  if (left !== ".") {
-    answer = middle(map[left], answer);
-  }
-  //middle
-  answer += root;
-
-  //right
-  if (right !== ".") {
-    answer = middle(map[right], answer);
-  }
-
-  return answer;
-};
-
-const result2 = middle(map["A"], "");
-console.log(result2);
-
-// 후위
-const post = (node, answer = "") => {
-  const root = node.root;
-  const left = node.left;
-  const right = node.right;
-
-  //left
-  if (left !== ".") {
-    answer = post(map[left], answer);
-  }
-
-  //right
-  if (right !== ".") {
-    answer = post(map[right], answer);
-  }
-
-  //middle
-  answer += root;
-
-  return answer;
-};
-
-const result3 = post(map["A"], "");
-console.log(result3);
+console.log(traverse("pre", "A").join(""));
+console.log(traverse("in", "A").join(""));
+console.log(traverse("post", "A").join(""));
